fcc97fbdfd3d173c2938abc4f512dfcb5c8cdba3
karttur <thomas.gumbricht@gmail.com>
2021-02-09 16:46:18 +0100

    initial commit

---
+++ add_submodules.sh
@@ -0,0 +1,15 @@
+git submodule add --name ancillary https://github.com/karttur/geoimagine02-ancillary karttur_v202003/geoimagine/ancillary
+git submodule add --name gis https://github.com/karttur/geoimagine02-gis karttur_v202003/geoimagine/gis
+git submodule add --name ktpandas https://github.com/karttur/geoimagine02-ktpandas karttur_v202003/geoimagine/ktpandas
+git submodule add --name landsat https://github.com/karttur/geoimagine02-landsat karttur_v202003/geoimagine/landsat
+git submodule add --name layout https://github.com/karttur/geoimagine02-layout karttur_v202003/geoimagine/layout
+git submodule add --name modis https://github.com/karttur/geoimagine02-modis karttur_v202003/geoimagine/modis
+git submodule add --name params https://github.com/karttur/geoimagine02-params karttur_v202003/geoimagine/params
+git submodule add --name postgresdb https://github.com/karttur/geoimagine02-postgresdb karttur_v202003/geoimagine/postgresdb
+git submodule add --name projects https://github.com/karttur/geoimagine02-projects karttur_v202003/geoimagine/projects
+git submodule add --name sentinel https://github.com/karttur/geoimagine02-sentinel karttur_v202003/geoimagine/sentinel
+git submodule add --name setup_db https://github.com/karttur/geoimagine02-setup_db karttur_v202003/geoimagine/setup_db
+git submodule add --name setup_process https://github.com/karttur/geoimagine02-setup_process karttur_v202003/geoimagine/setup_process
+git submodule add --name smap https://github.com/karttur/geoimagine02-smap karttur_v202003/geoimagine/smap
+git submodule add --name support https://github.com/karttur/geoimagine02-support karttur_v202003/geoimagine/support
+git submodule add --name updatedb https://github.com/karttur/geoimagine02-updatedb karttur_v202003/geoimagine/updatedb
